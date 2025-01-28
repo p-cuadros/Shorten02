8e52cd542cdf43650514ffb979d355f1afc7ebfd
PatCuadrosQ <patrick_jcq@hotmail.com>
2025-01-27 20:45:11 -0500

    v1

---
+++ infra/main.tf
@@ -0,0 +1,98 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+variable "suscription_id" {
+    type = string
+    description = "Azure subscription id"
+}
+
+variable "sqladmin_username" {
+    type = string
+    description = "Administrator username for server"
+}
+
+variable "sqladmin_password" {
+    type = string
+    description = "Administrator password for server"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.suscription_id
+}
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 100
+  max = 999
+}
+
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "upt-arg-${random_integer.ri.result}"
+  location = "eastus"
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "upt-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "upt-awa-${random_integer.ri.result}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  depends_on            = [azurerm_service_plan.appserviceplan]
+  //https_only            = true
+  site_config {
+    minimum_tls_version = "1.2"
+    always_on = false
+    application_stack {
+      dotnet_version = "8.0"
+    }
+  }
+}
+
+resource "azurerm_mssql_server" "sqlsrv" {
+  name                         = "upt-dbs-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sqladmin_username
+  administrator_login_password = var.sqladmin_password
+}
+
+resource "azurerm_mssql_firewall_rule" "sqlaccessrule" {
+  name             = "PublicAccess"
+  server_id        = azurerm_mssql_server.sqlsrv.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
+}
+
+resource "azurerm_mssql_database" "sqldb" {
+  name      = "shorten"
+  server_id = azurerm_mssql_server.sqlsrv.id
+  sku_name = "Free"
+}
+
+# resource "azurerm_sql_database" "sqldb" {
+#   name                = "shorten"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   server_name         = azurerm_sql_server.sqlsrv.name
+#   edition             = "Basic"
+#   requested_service_objective_name = "S0"
+# }
